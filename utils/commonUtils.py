c1fb919024b2e83339acb3caacc0f73499b28eb4
ram-meegada <ramumeegada1811@gmail.com>
2025-08-02 12:58:45 +0530

    #6 human readable changes

--- utils/commonUtils.py
+++ utils/commonUtils.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.mail import send_mail
 import logging
 from django.utils import timezone
-from datetime import timedelta, datetime
+from datetime import timedelta, datetime, date
 from django.db import connection
 import tracemalloc
 
@@ -61,6 +61,34 @@ def debugging_decorator(func):
     def wrapper(*args, **kwargs):
         start_time = datetime.now()
 
+        def bytes_to_human_readable(size_in_bytes):
+            for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
+                if size_in_bytes < 1024:
+                    return f"{size_in_bytes:.2f} {unit}"
+                size_in_bytes /= 1024
+            return f"{size_in_bytes:.2f} PB"
+
+        def convert_time_to_human_readable(total_time: datetime):
+            sec = total_time.total_seconds()
+            sec = int(sec)
+
+            hours, remainder = divmod(sec, 3600)
+            minutes, seconds = divmod(remainder, 60)
+
+            if not any((hours, minutes, seconds)):
+                return total_time
+
+            res = []
+
+            if hours:
+                res.append(f'{hours} hour{"s" if hours > 1 else ""}')
+            if minutes:
+                res.append(f'{minutes} minute{"s" if minutes > 1 else ""}')
+            if seconds:
+                res.append(f'{seconds} second{"s" if seconds > 1 else ""}')
+
+            return ' '.join(res)
+
         tracemalloc.start()
         mem_snapshot_one = tracemalloc.take_snapshot()
 
@@ -70,11 +98,13 @@ def debugging_decorator(func):
         tracemalloc.stop()
 
         stats = mem_snapshot_two.compare_to(mem_snapshot_one, 'lineno')
-        total_memory_taken = sum([stat.size_diff for stat in stats])
+        total_memory_taken = bytes_to_human_readable(sum([stat.size_diff for stat in stats]))
+
+        total_time_formatted = convert_time_to_human_readable(datetime.now() - start_time)
 
         logger.info(f'Number of queries taken, {len(connection.queries)}')
-        logger.info(f'Total Execution time, {datetime.now() - start_time}')
-        logger.info(f'Total memory taken, {total_memory_taken/1024} KB')
+        logger.info(f'Total Execution time, {total_time_formatted}')
+        logger.info(f'Total memory taken, {total_memory_taken}')
 
         return result
     return wrapper
